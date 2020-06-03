fc598abb25be23fbabf54d066a3310ff9dfeff04
Rafael Fernandes <rafael.londrina@gmail.com>
2020-06-03 14:41:50 -0300

    Fisrt commit Server

---
+++ server/app/Models/Team.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const Model = use('Model')
+
+class Team extends Model {
+  static boot () {
+    super.boot()
+
+    this.addTrait('@provider:Lucid/Slugify', {
+      fields: {
+        slug: 'name'
+      },
+      strategy: 'dbIncrement',
+      disableUpdates: false
+    })
+  }
+
+  users () {
+    return this.belongsToMany('App/Models/User').pivotModel(
+      'App/Models/UserTeam'
+    )
+  }
+}
+
+module.exports = Team
