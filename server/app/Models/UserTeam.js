04a03306285b808cd144bbac309e72096d241b9c
Rafael Fernandes <rafael.londrina@gmail.com>
2020-06-03 17:09:51 -0300

    Server: Setup Autorização

--- server/app/Models/UserTeam.js
+++ server/app/Models/UserTeam.js
@@ -3,6 +3,21 @@
 const Model = use('Model')
 
 class UserTeam extends Model {
+  static get traits () {
+    return [
+      '@provider:Adonis/Acl/HasRole',
+      '@provider:Adonis/Acl/HasPermission'
+    ]
+  }
+
+  roles () {
+    return this.belongsTo('Adonis/Acl/Role')
+  }
+
+  perission () {
+    return this.belongsTo('Adonis/Acl/Permission')
+  }
+
   user () {
     return this.belongsTo('App/Models/User')
   }
